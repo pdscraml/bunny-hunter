de8fb782eaf7b44537bdd0a1d4a48208b5d2b888
Ian Wakely <raveious.irw@gmail.com>
2017-04-22 21:19:20 -0400

    Updating rviz to match new heading.

--- catkin_ws/src/easter_egg_hunt/scripts/discovery.py
+++ catkin_ws/src/easter_egg_hunt/scripts/discovery.py
@@ -5,72 +5,55 @@ import smach_ros
 import sys
 import tf
 
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseArray
+from std_msgs.msg import Header
 from visualization_msgs.msg import MarkerArray, Marker
 from ar_track_alvar_msgs.msg._AlvarMarkers import AlvarMarkers
 
-
-
 class WaypointManager(object):
     _waypoints = {}
+    _marker_filter = (1,2,3)
     def __init__(self, marker_distance=1):
         rospy.init_node("WaypointManager", anonymous=False)
         self.marker_distance = marker_distance
         self.marker_callback = self.saveMarkerWaypoint
         self.tf_list = tf.TransformListener()
 
-        viz_pub = rospy.Publisher("WaypointManager/waypoint_visualization", MarkerArray, queue_size=10)
+        viz_pub = rospy.Publisher("WaypointManager/waypoints", PoseArray, queue_size=10)
         viz_update = rospy.Rate(2)
 
         sub = rospy.Subscriber("/ar_pose_marker", AlvarMarkers, self.callback)
 
         while not rospy.is_shutdown():
-            mark_viz = []
-            for ID in self._waypoints:
-                temp = Marker()
+            header = Header(frame_id='/map')
+            viz_pub.publish(PoseArray(header, self._waypoints.values()))
 
-                temp.id = ID
-                temp.action = Marker.ADD
-                temp.ns = "WaypointManager"
-                temp.type = Marker.ARROW
-                temp.pose = self._waypoints[ID].pose
-                temp.header = self._waypoints[ID].header
-
-                temp.scale.x = 0.02
-                temp.scale.y = 0.5
-                temp.scale.z = 0.02
-
-                temp.color.r = 1
-                temp.color.g = 0
-                temp.color.b = 0
-                temp.color.a = 1
-
-                mark_viz.append(temp)
-
-            viz_pub.publish(markers=mark_viz)
-            print("marker update {}".format(self._waypoints.keys()))
+            rospy.loginfo("marker update {}".format(self._waypoints.keys()))
             viz_update.sleep()
 
     def saveMarkerWaypoint(self, marker):
-        trans = None
-        rot = None
-
         if self.tf_list.canTransform('map', 'head_camera', rospy.Time(0)):
             pose_st = PoseStamped()
             pose_st = marker.pose
+            pose_st.pose.orientation.x = 0
+            pose_st.pose.orientation.z = 0
+            pose_st.pose.orientation.w = pose_st.pose.orientation.y
             pose_st.header.frame_id = 'head_camera'
 
             transd_pose = self.tf_list.transformPose("map", pose_st)
 
-            self._waypoints[marker.id] = transd_pose
+            self._waypoints[marker.id] = transd_pose.pose
 
         else:
             rospy.logwarn("Had problems transforming the waypoint.")
 
     def callback(self, data):
         for marker in data.markers:
-            # print(marker)
-            self.marker_callback(marker)
+            if marker.id in self._marker_filter:
+                # print(marker)
+                self.marker_callback(marker)
+            else:
+                rospy.logwarn("Marker {} was filtered out...".format(marker.id))
 
 # standard ros boilerplate
 if __name__ == "__main__":
