951ba52539c853ef1f2344525dc66af13d3d27fb
amkurup <amkurup@mtu.edu>
2017-04-23 19:04:05 -0400

    updating exploration code

--- catkin_ws/src/easter_egg_hunt/scripts/wall_follow.py
+++ catkin_ws/src/easter_egg_hunt/scripts/wall_follow.py
@@ -6,7 +6,7 @@
 #               Ian
 #              Akhil
 #
-# Revision: v1.2
+# Revision: v1.3
 
 # imports
 import rospy
@@ -24,7 +24,10 @@ import smach_ros
 import numpy as np
 
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point, Quaternion
+from nav_msgs.msg import MapMetaData, Odometry
+from move_base_msgs.msg import MoveBaseGoal, MoveBaseAction
+
 
 # wall following routine
 class wallFollow(smach.State):
@@ -42,12 +45,12 @@ class wallFollow(smach.State):
 
         # define speed and turning coefficients
         self.turnCoef = [(x ** 2 - 8750) / 5000000.0 for x in xrange(-90, 0)] + [(-x ** 2 + 8750) / 5000000.0 for x in xrange(0, 91)]
-        self.speedCoef = [(-x ** 2 + 9900) / 7500000.0 for x in xrange(-90,91)]
+        self.speedCoef = [(-x ** 2 + 8750) / 7500000.0 for x in xrange(-90,91)]
         # define random spin time
-        self.rand = random.randrange(120, 150)
+        self.rand = random.randrange(100, 140)
 
-        # subscribers and publishers
-        rospy.Subscriber("/scan", LaserScan, self.function)
+        self.origin_pose = rospy.Subscriber("/odometry/filtered", Odometry, self.origin_detect)
+
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
         rospy.spin()
@@ -57,8 +60,43 @@ class wallFollow(smach.State):
             return 'EXPLORATION_COMPLETE'
         else:
             return 'EXPLORATION_INCOMPLETE'
-        if self.k >= 3:
-            return 'EXPLORATION_COMPLETE'
+
+
+    def origin_detect(self, data):
+        print('origin:')
+        self.origin = data.pose.pose
+        self.origin_pose.unregister()
+        print self.origin
+
+        # subscribe to laserscan
+        self.lidar = rospy.Subscriber("/scan", LaserScan, self.function)
+
+
+    def start_goal(self, data):
+        # define destination as move base
+        dest = []
+        dest = MoveBaseGoal()
+        # set initial position as goal
+        dest.target_pose.header.frame_id = 'map'
+        dest.target_pose.header.stamp = rospy.Time.now()
+        # dest.target_pose.pose.position = Point(0.0, 0.0, 0.0)
+        # dest.target_pose.pose.orientation = Quaternion(0.0, 0.0, 0.7, 0.7)
+
+        dest.target_pose.pose = self.origin
+
+        print('dest')
+        print dest.target_pose.pose
+
+        # actionlib as client for move base
+        mvbs = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        mvbs.wait_for_server()
+        # set move_base goal
+        mvbs.send_goal(dest)
+        mvbs.wait_for_result()
+        # mvbs.cancel_goal()
+        rospy.loginfo('Back to the Start!')
+
+        return 'EXPLORATION_COMPLETE'
 
     # function to switch between states
     def function(self, data):
@@ -73,8 +111,6 @@ class wallFollow(smach.State):
 
     # spin jackal 360 deg
     def jackalSpin(self, data):
-        print ('jackalSpin')
-
         # publish spin msg
         self.cmd.linear.x = 0
         self.cmd.angular.z = 0.45
@@ -82,11 +118,16 @@ class wallFollow(smach.State):
         # increment j on every iteration
         self.j += 1
         # reset 'i' to get out of spin
-        if self.j > 123:
+        if self.j > 135:
             self.i = 0
             self.k += 1
 
-        print (self.k)
+        # print ('jackalSpin')
+        # print (self.k)
+
+        if self.k >= 4:
+            self.lidar.unregister()
+            self.start_goal(data)
 
 
     def Callback(self, data):
@@ -126,7 +167,7 @@ class wallFollow(smach.State):
                     turnVal = turnVal + (self.turnCoef[p] * data.ranges[p])
 
             speedVal = min(speedVal * 1.2, 0.35) # sets max speed
-            turnVal = turnVal * 1.5
+            turnVal = turnVal * 1
 
             if front_zone_avg < 2.2:
                 turnVal = turnVal * 2.0
@@ -135,8 +176,8 @@ class wallFollow(smach.State):
         # rospy.loginfo('left_zone_avg:'+str(left_zone_avg))
         # rospy.loginfo('right_zone_avg:'+str(right_zone_avg))
 
-        rospy.loginfo('SpeedVal: ' + str(speedVal))
-        rospy.loginfo('turnVal: ' + str(turnVal))
+        # rospy.loginfo('SpeedVal: ' + str(speedVal))
+        # rospy.loginfo('turnVal: ' + str(turnVal))
 
         self.cmd = Twist()
         self.cmd.linear.x = speedVal
